fd335a812af1c861e014fdbdbb81ecc8e3475678
Nahuel-Ramirez <nahuel-ramirez@hotmail.com>
2022-06-21 20:55:53 -0300

    se estilo la pagina de detalles

--- src/Components/Detalle.js
+++ src/Components/Detalle.js
@@ -1,5 +1,5 @@
 import React, { useEffect, useState } from "react";
-import { Link, useNavigate } from "react-router-dom";
+import { useNavigate, Link } from "react-router-dom";
 import axios from "axios";
 
 // Material UI
@@ -8,7 +8,12 @@ import CardContent from "@mui/material/CardContent";
 import Typography from "@mui/material/Typography";
 import CardActions from "@mui/material/CardActions";
 import Button from "@mui/material/Button";
+import Box from "@mui/material/Box";
+import Divider from "@mui/material/Divider";
 import { grey } from "@mui/material/colors";
+
+// Style
+import "./detalle.css";
 
 function Detalle() {
   let token = sessionStorage.getItem("token");
@@ -38,49 +43,67 @@ function Detalle() {
       });
   }, [movieID, setMovieDetail]);
 
-  console.log(movieDetail);
-
   return (
     <>
       {movieDetail && (
-        <Container maxWidth="xs">
-          <img
-            title="movieIMG"
-            src={`https://image.tmdb.org/t/p/w400/${movieDetail.poster_path}`}
-            alt="movie poster"
-          />
-          <CardContent>
-            <Typography variant="h4" color="initial">
-              Detalle de {movieDetail.title}
-            </Typography>
-            <Typography variant="h6" color="initial">
-              {movieDetail.overview}
-            </Typography>
-            <Typography variant="body2" color="initial">
-              Generos:
-              {movieDetail.genres.map((gen, index) => {
-                return (
-                  <div key={index}>
-                    <ul>
-                      <li>{gen.name}</li>
-                    </ul>
-                  </div>
-                );
-              })}
-            </Typography>
-            <Typography variant="body2" color="initial">
-              <p>Año:</p>
-              {movieDetail.release_date}
-            </Typography>
-          </CardContent>
-          <CardActions>
-            <Button variant="outlined" sx={{ color: grey[900] }}>
-              <a href={movieDetail.homepage} target="_BLANK" rel="noreferrer">
-                Home Page
-              </a>
-            </Button>
-          </CardActions>
-        </Container>
+        <div className="detail-container">
+          <Box className="img-detail">
+            <img
+              title="movieIMG"
+              src={`https://image.tmdb.org/t/p/w400/${movieDetail.poster_path}`}
+              alt="movie poster"
+            />
+          </Box>
+          <Box className="body-detail">
+            <CardContent className="overview">
+              <Typography variant="h4">{movieDetail.title}</Typography>
+              <span className="divider-black"></span>
+              <Typography variant="h6">{movieDetail.overview}</Typography>
+            </CardContent>
+
+            <CardContent className="overview-data">
+              <Box className="data-movie">
+                <Typography variant="overline" component="h4">
+                  Generos:
+                </Typography>
+                <Typography variant="body2" color="initial">
+                  {movieDetail.genres.map((gen, index) => {
+                    return (
+                      <p className="gen" key={index}>
+                        {gen.name}.
+                      </p>
+                    );
+                  })}
+                </Typography>
+                <span className="divider"></span>
+                <Typography variant="overline" component="h4">
+                  Año:
+                </Typography>
+                <Typography variant="body2" color="initial">
+                  {movieDetail.release_date}
+                </Typography>
+              </Box>
+            </CardContent>
+
+            <CardActions className="home-page">
+              <Button variant="outlined" sx={{ color: grey[900] }}>
+                <a
+                  href={movieDetail.homepage}
+                  target="_BLANK"
+                  rel="noreferrer"
+                  className="btn-hp"
+                >
+                  Home Page
+                </a>
+              </Button>
+              <Button variant="outlined" sx={{ color: grey[900] }}>
+                <Link to="/listado" className="btn-hp">
+                  Volver al listado
+                </Link>
+              </Button>
+            </CardActions>
+          </Box>
+        </div>
       )}
     </>
   );
